699b9b45aae5ffd5553b6c19d8697bdeb9aae7eb
Jayesh Khurana <jayeshkhurana685@gmail.com>
2021-11-24 13:02:41 +0530

    intro to binary heap

---
+++ binaryHeap.cpp
@@ -0,0 +1,55 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+/* A Heap is a Tree-based data structure, which satisfies the below properties:
+A Heap is a complete tree (All levels are completely filled except possibly the last level and the last level has all keys as left as possible).
+
+A Heap is either Min Heap or Max Heap. In a Min-Heap, the key at root must be minimum among all keys present in the Binary Heap. The same property must be recursively true for all nodes in the Tree. Max Heap is similar to MinHeap.
+
+Binary Heap: A Binary Heap is a heap where each node can have at most two children. In other words, a Binary Heap is a complete Binary Tree satisfying the above-mentioned properties.
+
+Since a Binary Heap is a complete Binary Tree, it can be easily represented using Arrays.
+The root element will be at Arr[0].
+Below table shows indexes of other nodes for the ith node, i.e., Arr[i]:
+Arr[(i-1)/2]	Returns the parent node
+Arr[(2*i)+1]	Returns the left child node
+Arr[(2*i)+2]	Returns the right child node
+*/
+
+class myHeap {
+    int* arr;
+    int size;
+    int capacity;
+
+    myHeap (int c) {
+        arr = new int [c];
+        size = 0;
+        capacity = c;
+    }
+
+    int getLeft (int i) { return (2*i + 1); }
+    int getRight (int i) { return (2*i + 2); }
+    int getParent (int i) { return (i-1)/2; }
+
+    void insert (int x) {
+        if (size == capacity)
+            cout << "Heap is full" << endl;
+            return;
+        int element = size;
+        arr[size++] = x;
+
+        while (element != 0) {
+            int parent = getParent(element);
+            if (arr[element] < arr[parent])
+                break;
+            else {
+                swap(arr[element], arr[parent]);
+                element = parent;
+            }
+        }
+    }
+};
+
+int main () {
+
+}
