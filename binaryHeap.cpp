5b28432174ea88c772329f3f308481b29fa39456
Jayesh Khurana <jayeshkhurana685@gmail.com>
2022-01-12 00:45:19 +0530

    heap sort

--- binaryHeap.cpp
+++ binaryHeap.cpp
@@ -117,7 +117,35 @@ class myHeap {
     }
     // O(n); time complexity calculation involves maths, check video
 
-    
+    // heap sort
+    // selection sort using a heap
+    // in selection sort we find the minimum element after each iteration; in heap sort we find minimum element using heap
+    // to sort array in increasing order, we use max heap
+
+    void maxHeapify (int* arr, int n, int i) {
+        int largest=i, left=2*i+1, right=2*i+2;
+        if (left < n && arr[largest] < arr[left])
+            largest = left;
+        if (right < n && arr[largest] < arr[right])
+            largest = right;
+        if (largest != i) {
+            swap(arr[i], arr[largest]);
+            maxHeapify(arr, n, largest);
+        }
+    }
+
+    void buildHeap (int* arr, int n) {
+        for (int i=(n-2)/2; i>=0; i--)
+            maxHeapify(arr, n, i);
+    }
+
+    void heapSort (int* arr, int n) {
+        buildHeap(arr, n);
+        for (int i=n-1; i>=1; i--) {
+            swap(arr[0], arr[i]);
+            maxHeapify(arr, i, 0);
+        }
+    }
 };
 
 int main () {
