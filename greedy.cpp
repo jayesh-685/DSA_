441d41371e4922627c0cb09c0460f79d7ce0c161
Jayesh Khurana <jayeshkhurana685@gmail.com>
2022-01-28 13:25:11 +0530

    huffman coding

--- greedy.cpp
+++ greedy.cpp
@@ -51,6 +51,72 @@ int maxActivities (pair<int, int> arr[], int n) {
     return count;
 }
 
+// Huffman coding
+/* Huffman coding is a lossless data compression algorithm. The idea is to assign variable-length codes to input characters, lengths of the assigned codes are based on the frequencies of corresponding characters. The most frequent character gets the smallest code and the least frequent character gets the largest code.
+The variable-length codes assigned to input characters are Prefix Codes, means the codes (bit sequences) are assigned in such a way that the code assigned to one character is not the prefix of code assigned to any other character. This is how Huffman Coding makes sure that there is no ambiguity when decoding the generated bitstream. 
+Let us understand prefix codes with a counter example. Let there be four characters a, b, c and d, and their corresponding variable length codes be 00, 01, 0 and 1. This coding leads to ambiguity because code assigned to c is the prefix of codes assigned to a and b. If the compressed bit stream is 0001, the de-compressed output may be “cccd” or “ccb” or “acd” or “ab”.
+
+There are mainly two major parts in Huffman Coding:
+1. Build a Huffman Tree from input characters.
+2. Traverse the Huffman Tree and assign codes to characters.
+
+Steps to build Huffman Tree
+Input is an array of unique characters along with their frequency of occurrences and output is Huffman Tree. 
+
+Create a leaf node for each unique character and build a min heap of all leaf nodes (Min Heap is used as a priority queue. The value of frequency field is used to compare two nodes in min heap. Initially, the least frequent character is at root)
+Extract two nodes with the minimum frequency from the min heap.
+ 
+Create a new internal node with a frequency equal to the sum of the two nodes frequencies. Make the first extracted node as its left child and the other extracted node as its right child. Add this node to the min heap.
+Repeat steps#2 and #3 until the heap contains only one node. The remaining node is the root node and the tree is complete. */
+
+// huffman node
+struct hNode {
+    char ch;
+    int freq;
+    hNode* left;
+    hNode* right;
+    hNode (char c, int f, hNode *l=NULL, hNode *r=NULL) {
+        ch = c;
+        freq = f;
+        left = l;
+        right = r;
+    }
+};
+
+struct compare {
+    bool operator () (hNode* l, hNode* r) {
+        return l->freq < r->freq;
+    }
+};
+
+void printHCodes (char arr[], int freq[], int n) {
+    priority_queue <hNode*, vector<hNode*>, compare> pq;
+
+    for (int i=0; i<n; i++) {
+        pq.push(new hNode(arr[i], freq[i]));
+    }
+
+    while (pq.size() > 1) {
+        hNode* left = pq.top();
+        pq.pop();
+        hNode* right = pq.top();
+        pq.pop();
+        hNode* newNode = new hNode('$', left->freq+right->freq, left, right);
+        pq.push(newNode);
+    }
+}
+
+// see video
+void printCodes (hNode* root, string str) {
+    if (root->ch != '&') {
+        cout << root->ch << " : " << str << endl;
+        return;
+    }
+
+    printCodes(root->left, str+"0");
+    printCodes(root->right, str+"1");
+}
+
 int main () {
     pair<int, int> arr[] = {make_pair(12, 25), make_pair(10, 20), make_pair(20, 30)};
 
