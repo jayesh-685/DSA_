90a7b92d944335e646dabce17551b50985b3f232
Jayesh Khurana <jayeshkhurana685@gmail.com>
2022-01-31 19:01:52 +0530

    all

--- test1.cpp
+++ test1.cpp
@@ -1,15 +1,35 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-int main () {
-	long long n, m, a;
-	cin >> n >> m >> a;
+bool utility (vector<vector<int>> &grid, int i, int j, vector <vector <bool>> &visited 	) {
+	
+	if (i<0 || j<0 || i>=grid.size() || j>=grid[0].size() || visited[i][j] || grid[i][j]==0)
+		return false;
+	
+	if (grid[i][j] == 2)
+		return true;
 
-	long long p = max(n, m);
-	long long q = min(n, m);
-	long long x = ceil((double)p/a);
-	if (q-a > 0)
-		x += ceil((double)q/a);
+	visited[i][j] = true;
+	
+	if (utility(grid, i, j-1, visited) || utility(grid, i-1, j, visited) || utility(grid, i, j+1, visited) || utility(grid, i+1, j, visited))
+		return true;
+		
+	else
+		return false;
+}
 
-	cout << x;
+bool is_Possible(vector<vector<int>>& grid) 
+{
+	//code here
+	int r = grid.size();
+	int c = grid[0].size();
+	vector <vector <bool>> visited (r, vector<bool>(c, false));
+	
+	for (int i=0; i<r; i++) {
+		for (int j=0; j<c; j++) {
+			if (grid[i][j] == 1) {
+				return utility(grid, i, j, visited);
+			}
+		}
+	}
 }
