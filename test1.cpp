2785d120c80f36eed495342a209db1bd05a9583d
Jayesh Khurana <jayeshkhurana685@gmail.com>
2022-02-02 01:21:46 +0530

    test files

--- test1.cpp
+++ test1.cpp
@@ -33,3 +33,144 @@ bool is_Possible(vector<vector<int>>& grid)
 		}
 	}
 }
+
+struct MinHeapNode
+{
+	char data;
+	int freq;
+	MinHeapNode *left, *right;
+};
+
+
+//Function to return the decoded string.
+
+void huffmanDecode (struct MinHeapNode* root, string s, map <string, char> &m) {
+    if (root->data != '$') {
+        m.insert({s, root->data});
+        return;
+    }
+
+	huffmanDecode(root->left, s+"0", m);
+	huffmanDecode(root->right, s+"1", m);
+}
+
+string decodeHuffmanData(struct MinHeapNode* root, string binaryString)
+{
+    // Code here
+    map <string, char> m;
+    string s = "";
+    huffmanDecode(root, s, m);
+
+	string ans = "";
+	int i = 0, n = binaryString.length();
+
+	while (i < n) {
+		string str = "";
+		str += binaryString[i++];
+		auto it = m.find(str);
+		while (it == m.end()) {
+			str += binaryString[i++];
+			auto it = m.find(str);
+		}
+		ans += it->second;
+	}
+
+	return ans;
+
+}
+
+
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Item{
+    int value;
+    int weight;
+};
+
+
+ // } Driver Code Ends
+//class implemented
+/*
+struct Item{
+    int value;
+    int weight;
+};
+*/
+
+
+class Solution
+{
+    public:
+    //Function to get the maximum total value in the knapsack.
+    
+    struct triplet {
+        int val, weight;
+        double vpw;
+        triplet (int v, int w, double p) {
+            val = v; weight = w; vpw = p;
+        }
+    };
+    
+    struct compare {
+        bool operator () (triplet t1, triplet t2) {
+            return t1.vpw > t2.vpw;
+        } 
+    };
+    
+    double fractionalKnapsack(int W, Item arr[], int n)
+    {
+        // Your code here
+        vector <triplet> vec;
+        
+        for (int i=0; i<n; i++) {
+            vec.push_back(triplet(arr[i].value, arr[i].weight, (double)(arr[i].value/arr[i].weight)));
+        }
+        
+        sort(vec.begin(), vec.end(), compare());
+        
+        double ans = 0;
+        int i=0;
+        while (W && i<n) {
+            if (vec[i].weight <= W) {
+                W -= vec[i].weight;
+                ans += vec[i].val;
+                i++;
+            } else {
+                ans += (double)(vec[i].vpw * W);
+                W = 0;
+            }
+        }
+        
+        return ans;
+    }
+        
+};
+
+
+// { Driver Code Starts.
+int main()
+{
+	int t;
+	//taking testcases
+	cin>>t;
+	cout<<setprecision(2)<<fixed;
+	while(t--){
+	    //size of array and weight
+		int n, W;
+		cin>>n>>W;
+		
+		Item arr[n];
+		//value and weight of each item
+		for(int i=0;i<n;i++){
+			cin>>arr[i].value>>arr[i].weight;
+		}
+		
+		//function call
+		Solution ob;
+		cout<<ob.fractionalKnapsack(W, arr, n)<<endl;
+	}
+    return 0;
+}  // } Driver Code Ends
--- test2.cpp
+++ test2.cpp
@@ -1,10 +1,53 @@
-#include "bits/stdc++.h"
+// { Driver Code Starts
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-	
-	vector <vector <int>> v = {{1, 2, 3}, {4, 5, 6}, {7, 2, 9}};
-	cout << v.size();
 
+ // } Driver Code Ends
+// Function to return Largest Number
+
+class Solution
+{
+    public:
+    //Function to return the largest possible number of n digits
+    //with sum equal to given sum.
+    string largestNumber(int n, int sum)
+    {
+        // Your code here
+        string ans = "";
+        
+        if (sum > 9*n)
+			return "-1";
+
+		for (int i=0; i<n; i++) {
+			if (sum >= 9) {
+				ans += "9";
+			} else if (sum == 0) {
+				ans += "0";
+			} else {
+				ans += to_string(sum);
+				sum = 0;
+			}
+		}
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking testcases
+	int t;
+	cin>>t;
+
+	while(t--)
+	{
+	    //taking n and sum
+		int n,sum;
+		cin>>n>>sum;
+		
+        Solution obj;
+        //function call
+		cout<<obj.largestNumber(n, sum)<<endl;
+	}
 	return 0;
-}
+}  // } Driver Code Ends
