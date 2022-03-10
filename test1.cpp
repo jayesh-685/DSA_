97af063aff75e3739ff6c925171fd5786ac2d12f
Jayesh Khurana <jayeshkhurana685@gmail.com>
2022-03-10 17:14:25 +0530

    test

--- test1.cpp
+++ test1.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 
 int main () {
-	int x, y;
-	int a;
-	for (int i=0; i<5; i++) {
-		for (int j=0; j<5; j++) {
-			cin >> a;
-			if (a) {
-				x=i; y=j;
-			}
+	int T;
+	cin >> T;
+	while (T--) {
+		int l, r, a;
+		cin >> l >> r >> a;
+		int res = 0;
+		if (l/a == r/a) {
+			res = (r/a) + (r%a);
+		} else {
+			res = max((r/a + r%a), (r/a -1 + a-1));
 		}
-	}
 
-	cout << abs(x-2) + abs(y-2);
+		cout << res << endl;	
+	}
+	
 }
--- test2.cpp
+++ test2.cpp
@@ -2,52 +2,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
- // } Driver Code Ends
-// Function to return Largest Number
-
-class Solution
-{
-    public:
-    //Function to return the largest possible number of n digits
-    //with sum equal to given sum.
-    string largestNumber(int n, int sum)
-    {
-        // Your code here
-        string ans = "";
-        
-        if (sum > 9*n)
-			return "-1";
-
-		for (int i=0; i<n; i++) {
-			if (sum >= 9) {
-				ans += "9";
-			} else if (sum == 0) {
-				ans += "0";
-			} else {
-				ans += to_string(sum);
-				sum = 0;
-			}
-		}
-    }
-};
-
-// { Driver Code Starts.
-int main()
-{
-    //taking testcases
-	int t;
-	cin>>t;
-
-	while(t--)
-	{
-	    //taking n and sum
-		int n,sum;
-		cin>>n>>sum;
-		
-        Solution obj;
-        //function call
-		cout<<obj.largestNumber(n, sum)<<endl;
-	}
-	return 0;
-}  // } Driver Code Ends
+int main () {
+	cout << floor(1000000000/1);
+}
