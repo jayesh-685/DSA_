9a7fddab183dc12d91849bfc7c36e0bbe3336e12
Jayesh Khurana <jayeshkhurana685@gmail.com>
2022-03-16 23:05:58 +0530

    codeforces

--- test1.cpp
+++ test1.cpp
@@ -1,28 +1,6 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-string solve (int n) {
-	string ans;
-	string subStr;
-	int x = n/3;
-	if (n%3 == 0 || n%3 == 2) {
-		subStr = "21";
-	} else {
-		subStr = "12";
-	}
-
-	for (int i=1; i<=x; i++) {
-		ans += subStr;
-	}
-
-	if (n%3 == 2) {
-		ans += '2';
-	} else if (n%3 == 1) {
-		ans += '1';
-	}
-
-	return ans;
-}
 
 int main () {
 	int T;
@@ -30,6 +8,30 @@ int main () {
 	while (T--) {
 		int n; 
 		cin >> n;
-		cout << solve(n) << endl;
+		vector <int> v (n);
+		for (int i=0; i<n; i++)
+			cin >> v[i];
+		
+		sort(v.begin(), v.end());
+		
+		int prefix = v[0], suffix = 0;
+		int i=1, j=n-1;
+		bool flag = false;
+		while (i<j) {
+			if (prefix < suffix) {
+				flag = true;
+				break;
+			} else {
+				prefix += v[i];
+				suffix += v[j];
+				i++;
+				j--;
+			}
+		}
+
+		if (flag)
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
 	}
 }
--- test2.cpp
+++
@@ -1,7 +0,0 @@
-// { Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-int main () {
-	cout << floor(1000000000/1);
-}
