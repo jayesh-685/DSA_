32893cc215fceae10719391a37fbc4a5737b27f2
Jayesh Khurana <jayeshkhurana685@gmail.com>
2022-03-20 12:24:10 +0530

    kickstart

--- test1.cpp
+++ test1.cpp
@@ -1,37 +1,47 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+int lcs (string s1, string s2) {
+    int m = s1.length(), n = s2.length();
+    int dp[m+1][n+1];
+    
+    for(int i=0;i<=m;i++)
+        dp[i][0]=0;
+    
+    for(int j=0;j<=n;j++)
+        dp[0][j]=0;
+        
+    for (int i=1; i<=m; i++) {
+        for (int j=1; j<=n; j++) {
+            if(s1[i-1]==s2[j-1])
+                dp[i][j] = 1 + dp[i-1][j-1];
+            else
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+        }
+    }
+    return dp[m][n];
+}
 
 int main () {
 	int T;
 	cin >> T;
+	int i=1;
 	while (T--) {
-		int n; 
-		cin >> n;
-		vector <int> v (n);
-		for (int i=0; i<n; i++)
-			cin >> v[i];
-		
-		sort(v.begin(), v.end());
-		
-		int prefix = v[0], suffix = 0;
-		int i=1, j=n-1;
-		bool flag = false;
-		while (i<j) {
-			if (prefix < suffix) {
-				flag = true;
-				break;
-			} else {
-				prefix += v[i];
-				suffix += v[j];
-				i++;
-				j--;
-			}
+		string s1, s2;
+		cin >> s1 >> s2;
+		bool flag = true;
+		if (s2.length() < s1.length()) {
+			flag = false;
+		} else {
+			if (lcs(s1, s2) != s1.length())
+				flag = false;
 		}
 
-		if (flag)
-			cout << "YES" << endl;
-		else
-			cout << "NO" << endl;
+		if (flag) {
+			cout << "Case #" << i << ": " << s2.length() - s1.length() << endl;
+		} else {
+			cout << "Case #" << i << ": " << "IMPOSSIBLE" << endl;
+		}
+		i++;
 	}
 }
