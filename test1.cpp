4d8e049d2ebbe6333219ec2d809402133441c033
Jayesh Khurana <jayeshkhurana685@gmail.com>
2022-03-23 21:18:49 +0530

    codechef starters

--- test1.cpp
+++ test1.cpp
@@ -1,47 +1,8 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-bool isRegular (string s) {
-	stack <int> st;
-	for (int i=0; i<s.length(); i++) {
-		if (s[i] == '(') {
-			st.push(1);
-		} else {
-			if (st.empty())
-				return false;
-			st.pop();
-		}
-	}
-	return st.empty();
-}
+int solve (string s) {
 
-bool isPanlindrome (string s) {
-	int n = s.length();
-	for (int i=0; i<s.length()/2; i++) {
-		if (s[i] != s[n-i-1])
-			return false;
-	}
-
-	return true;
-}
-
-pair<int, int> solve (string &s) {
-	int operations = 0;
-	while (true) {
-		bool possible = false;
-		for (int i=1; i<s.length(); i++) {
-			if (isRegular(s.substr(0, i+1)) || isPanlindrome(s.substr(0, i+1))) {
-				s = s.substr(i+1);
-				operations++;
-				possible = true;
-				break;
-			}
-		}
-		if (!possible)
-			return {operations, s.length()};
-	}
-
-	return {-1, -1};
 }
 
 int main () {
@@ -50,9 +11,13 @@ int main () {
 	while (T--) {
 		int n;
 		cin >> n;
-		string s;
-		cin >> s;
-		pair<int, int> ans = solve(s);
-		cout << ans.first << " " << ans.second << endl;
+		if (n%2 == 0)
+			cout << n/2 << endl;
+		else {
+			n /= 2;
+			n += 1;
+			n *= -1;
+			cout << n << endl;
+		}
 	}
 }
