68eb4ec4842cec7f9cafe155b89c69f1555b0a9e
jayesh-685 <73791506+jayesh-685@users.noreply.github.com>
2022-04-20 08:58:52 +0530

    min coin change

--- dynamicProgramming.cpp
+++ dynamicProgramming.cpp
@@ -430,6 +430,30 @@ int mcn (vector <int> v) {
     return mcn[0][mcn.size()-1];
 }
 
+int coinChangeMin (vector <int> coins, int amount) {
+    int n = coins.size();
+    vector <vector <int>> dp (n+1, vector <int> (amount+1));
+
+    for (int i=0; i<=amount; i++)
+        dp[0][i] = INT_MAX - 1;
+
+    for (int i=0; i<=coins.size(); i++) {
+          dp[i][0] = 0;
+    }
+
+    for (int i=1; i<=coins.size(); i++) {
+        for (int j=1; j<=amount; j++) {
+            if (j >= coins[i-1]) {
+                dp[i][j] = min(dp[i-1][j], 1+dp[i][j-coins[i-1]]);
+            } else {
+                dp[i][j] = dp[i-1][j];
+            }
+        }
+    }
+
+    return dp[coins.size()][amount] == INT_MAX - 1 ? -1 : dp[coins.size()][amount]  ;
+}
+
 // maximum sum with no two consecutive 
 int main () {
     // cout << fibo(5) << endl;
